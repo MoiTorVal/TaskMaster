f10de03a377e18587170dd5f2770cc91d0d82274
moises valdez <moisesvaldez0559@gmail.com>
2025-01-05 12:15:20 -0800

    Connect to MongoDB and clean up code

---
+++ backend/models/Task.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true, // Title is a required field
+  },
+  description: {
+    type: String,
+    required: true, // Description is a required field
+  },
+  dueDate: {
+    type: Date,
+    required: true, // Due date is required
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"], // Enum to specify valid priority values
+    default: "Medium", // Default priority is Medium
+  },
+  completed: {
+    type: Boolean,
+    default: false, // Default status is false (not completed)
+  },
+});
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
+// Import third-party libraries first
 const express = require("express");
-const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
+// Set up constants and configurations
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error: ", err));
+
+// Middleware configuration
+app.use(cors()); // enables cross-origin resource sharing
+app.use(express.json()); // Parses incoming JSON request
+
+// routes
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+// server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
