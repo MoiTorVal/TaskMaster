44085d08cd1336749947424148f89a5e54042b99
moises valdez <moisesvaldez0559@gmail.com>
2025-01-05 22:44:01 -0800

    CRUD routes were made

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 const app = express();
 
 const Task = require("../models/Task");
@@ -7,8 +8,7 @@ const Task = require("../models/Task");
 // Middleware to parse incoming JSON data
 router.use(express.json());
 
-// define route handlers
-
+// POST
 router.post("/", async (req, res) => {
   const { title, description, dueDate, priority, completed } = req.body;
 
@@ -25,6 +25,80 @@ router.post("/", async (req, res) => {
     res.status(201).json(newTask);
   } catch (error) {
     res.status(400).json({ error: error.message });
+  }
+});
+
+// GET
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// GET with :ID
+router.get("/:id", async (req, res) => {
+  const taskId = req.params.id;
+
+  try {
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+    res.status(200).json(task); // Only send the response here
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// PATCH with :ID
+router.patch("/:id", async (req, res) => {
+  const taskId = req.params.id;
+  const updates = req.body;
+
+  // validate the task ID
+  if (!mongoose.isValidObjectId(taskId)) {
+    return res.status(404).json({ error: "Invalid task ID" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(taskId, updates, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      // If the task is not found, send a 404 response
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.status(200).json(task); // Only send the response here
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// DELETE with :id
+router.delete("/:id", async (req, res) => {
+  const taskId = req.params.id;
+
+  // Validate the task ID
+  if (!mongoose.isValidObjectId(taskId))
+    return res.status(404).json({ error: "Invalid task ID" });
+
+  try {
+    const task = await Task.findByIdAndDelete(taskId);
+
+    if (!task) {
+      // If the task is not found, send a 404 response
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
